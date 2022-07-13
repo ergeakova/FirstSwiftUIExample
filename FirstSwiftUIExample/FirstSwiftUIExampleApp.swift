56f52ed5fdd79c3c85804082bbbbdad0e676f348
ergeakova <ergeak@gmail.com>
2022-07-13 13:59:48 +0300

    Initial Commit

---
+++ FirstSwiftUIExample/FirstSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIExampleApp.swift
+//  FirstSwiftUIExample
+//
+//  Created by Erge Gevher Akova on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
